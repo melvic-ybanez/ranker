f18f4eacd75280451b47324cd3fa849d020a1ddd
Melvic Ybanez <melvic.ybanez2014@gmail.com>
2022-02-20 21:46:34 +0800

    Add basic operations for topic

--- topics-management/src/main/scala/com/melvic/ranker/topics/services/TopicService.scala
+++ topics-management/src/main/scala/com/melvic/ranker/topics/services/TopicService.scala
@@ -1,9 +1,28 @@
 package com.melvic.ranker.topics.services
 
-trait TopicService {
+import cats.free.Free
+import com.melvic.ranker.models.Topic
+import com.melvic.ranker.topics.services.TopicService.{Action, AddTopic, GetTopic, ListTopics}
+import com.melvic.ranker.utils.Page
+import com.melvic.ranker.implicits._
 
+trait TopicService {
+  def addTopic(topic: Topic): Action[Unit] =
+    AddTopic(topic)
+
+  def getTopic(topicId: Topic.Id): Action[Topic] =
+    GetTopic(topicId)
+
+  def listTopic(predicate: Topic => Boolean, page: Page): Action[List[Topic]] =
+    ListTopics(predicate, page)
 }
 
 object TopicService {
   sealed trait ActionF[A]
+
+  final case class AddTopic(topic: Topic) extends ActionF[Unit]
+  final case class GetTopic(topicId: Topic.Id) extends ActionF[Topic]
+  final case class ListTopics(predicate: Topic => Boolean, page: Page) extends ActionF[List[Topic]]
+
+  type Action[A] = Free[ActionF, A]
 }

096dc9652cea29149c7db8c943e3c1d283fcd05c
Melvic Ybanez <melvic.ybanez2014@gmail.com>
2022-02-25 23:23:18 +0800

    Initial commit

---
+++ src/test/java/com/melvic/ranker/RankerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.melvic.ranker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RankerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
